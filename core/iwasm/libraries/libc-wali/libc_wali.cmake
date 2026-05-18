301f4794a0653fcb8f5883d78158163eb3ae71d8
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2026-05-18 15:34:59 -0400

    Rename to arch

--- core/iwasm/libraries/libc-wali/libc_wali.cmake
+++ core/iwasm/libraries/libc-wali/libc_wali.cmake
@@ -6,11 +6,11 @@ set (ARCH ${CMAKE_SYSTEM_PROCESSOR})
 
 add_definitions (-DWASM_ENABLE_LIBC_WALI=1)
 
-include_directories(${LIBC_WALI_DIR}/wali_arch/${ARCH})
+include_directories(${LIBC_WALI_DIR}/arch/${ARCH})
 
 # Setup files to compile
 file (GLOB_RECURSE wali_arch_sources 
-    ${LIBC_WALI_DIR}/wali_arch/${ARCH}/*.s 
+    ${LIBC_WALI_DIR}/arch/${ARCH}/*.s 
     )
 set (WALI_SOURCES ${LIBC_WALI_DIR}/wali.c ${LIBC_WALI_DIR}/impl.c ${LIBC_WALI_DIR}/copy.c ${wali_arch_sources})
 
